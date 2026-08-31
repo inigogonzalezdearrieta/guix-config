b32a14220d6b3d5f2d9de4b089eeeba12b6e4e45
Inigo Gonzalez de Arrieta <inigo.gonzalezdearrieta@ehu.eus>
2026-08-31 12:31:14 +0200

    Added a home config that runs on a container

---
+++ home.scm
@@ -0,0 +1,46 @@
+(use-modules (gnu home)
+             (gnu home services)
+             (gnu home services shells)
+             (gnu home services xdg)
+             (gnu services)
+	     (gnu packages emacs)
+	     (gnu packages text-editors)
+             (guix gexp))
+
+(home-environment
+  ;; Packages installed in the home profile.
+  (packages
+   (list
+    emacs
+    nano))
+
+  (services
+   (list
+    ;; Bash configuration
+    (service home-bash-service-type
+             (home-bash-configuration
+	      (guix-defaults? #t)
+	      (environment-variables '(("EDITOR" . "nano")))))
+
+    ;; Manually configured aliases
+    (simple-service
+     'bash-aliases-config
+     home-files-service-type
+     `((".bash_aliases" ,(local-file "dotfiles/bash_aliases"))))
+
+    ;; Nano configuration
+    (simple-service
+     'nanorc-config
+     home-files-service-type
+     `((".nanorc" ,(local-file "dotfiles/nanorc"))))
+
+    ;; Emacs configuration directory
+    (simple-service
+     'emacs-config
+     home-files-service-type
+     `((".emacs.d/init.el"
+         ,(local-file "emacs.d/init.el"))
+       (".emacs.d/early-init.el"
+        ,(local-file "emacs.d/early-init.el"))
+       (".emacs.d/custom.el"
+        ,(local-file "emacs.d/custom.el")))))))
