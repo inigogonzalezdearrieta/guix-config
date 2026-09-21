b3b546b754b7d181d663f93e6858e0beb852e607
inigogonzalezdearrieta <inigo.gonzalezdearrieta@ehu.eus>
2026-09-21 17:51:15 +0200

    Updated the channel and added nonguix repo

--- channels.scm
+++ channels.scm
@@ -2,9 +2,19 @@
        (name 'guix)
        (url "https://git.guix.gnu.org/guix.git")
        (branch "master")
-       (commit "ac43d7ed18a5372a7c87c5e5adc40f202363b956")
+       (commit "b27a72b79f648dce01c42fb323d732b4cf0cfdca")
        (introduction
         (make-channel-introduction
          "9edb3f66fd807b096b48283debdcddccfea34bad"
          (openpgp-fingerprint
           "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+(cons* (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        ;; Enable signature verification:
+        (introduction
+         (make-channel-introduction
+          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+          (openpgp-fingerprint
+           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+       %default-channels)
