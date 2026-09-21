08b032308785cb569a93f560cc551d18e2e4948b
inigogonzalezdearrieta <inigo.gonzalezdearrieta@ehu.eus>
2026-09-21 17:53:16 +0200

    Updated the home config to be consistent with the change to Guix System

--- home.scm
+++ home.scm
@@ -1,23 +1,25 @@
-(use-modules (gnu home)
-             (gnu home services)
-             (gnu home services shells)
-             (gnu home services xdg)
-             (gnu services)
-	     (gnu packages emacs)
-	     (gnu packages text-editors)
-             (guix gexp))
+(define-module (guix-home-config)
+  #:use-module (gnu home)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services shells)
+  #:use-module (gnu services)
+  #:use-module (gnu packages emacs)
+  #:use-module (gnu packages text-editors)
+  #:use-module (gnu system shadow)
+  #:use-module (guix gexp))
 
 (home-environment
   ;; Packages installed in the home profile.
   (packages
    (list
-    emacs
+    emacs-pgtk ;; With Wayland support
     nano))
 
   (services
-   (list
-    ;; Bash configuration
-    (service home-bash-service-type
+   (append
+    (list
+     ;; Bash configuration
+     (service home-bash-service-type
              (home-bash-configuration
 	      (guix-defaults? #t)
 	      (bashrc (list (local-file "dotfiles/bashrc"
@@ -25,31 +27,30 @@
 	      (bash-logout (list (local-file "dotfiles/bash_logout"
                                                "bash_logout")))
 	      (environment-variables '(("EDITOR" . "nano")))))
+    
+     ;; Dotfiles automatically created by Guix.
+     (service home-files-service-type
+           `((".guile" ,%default-dotguile)
+             (".Xdefaults" ,%default-xdefaults)))
+             
+     ;; Manually configured aliases
+     (simple-service
+      'bash-aliases-config
+      home-files-service-type
+      `((".bash_aliases" ,(local-file "dotfiles/bash_aliases"))))
 
-    ;; Manually configured aliases
-    (simple-service
-     'bash-aliases-config
-     home-files-service-type
-     `((".bash_aliases" ,(local-file "dotfiles/bash_aliases"))))
+     ;; Nano configuration
+     (simple-service
+      'nanorc-config
+      home-files-service-type
+      `((".nanorc" ,(local-file "dotfiles/nanorc"))))
 
-    ;; Nano configuration
-    (simple-service
-     'nanorc-config
-     home-files-service-type
-     `((".nanorc" ,(local-file "dotfiles/nanorc"))))
-
-    ;; Emacs configuration directory
-    (simple-service
-     'emacs-config
-     home-files-service-type
-     `((".emacs.d/init.el"
-         ,(local-file "emacs.d/init.el"))
-       (".emacs.d/early-init.el"
-        ,(local-file "emacs.d/early-init.el"))))
-
-    ;; Systemd services configuration
-    (simple-service
-     'systemd-services-config
-     home-files-service-type
-     `((".config/systemd/user/emacs.service"
-         ,(local-file "systemd/emacs.service")))))))
+     ;; Emacs configuration directory
+     (simple-service
+      'emacs-config
+      home-files-service-type
+      `((".emacs.d/init.el"
+          ,(local-file "emacs.d/init.el"))
+        (".emacs.d/early-init.el"
+        ,(local-file "emacs.d/early-init.el")))))
+      %base-home-services)))
