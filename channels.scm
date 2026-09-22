df44ac231f18f12673e144754e90caf8830ef5e2
inigogonzalezdearrieta <inigo.gonzalezdearrieta@ehu.eus>
2026-09-22 15:37:00 +0200

    Modified channel information to make guix-config a channel and point to thermomat/

--- channels.scm
+++ channels.scm
@@ -7,14 +7,17 @@
         (make-channel-introduction
          "9edb3f66fd807b096b48283debdcddccfea34bad"
          (openpgp-fingerprint
-          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
-(cons* (channel
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
         (name 'nonguix)
         (url "https://gitlab.com/nonguix/nonguix")
-        ;; Enable signature verification:
         (introduction
          (make-channel-introduction
           "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
           (openpgp-fingerprint
            "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-       %default-channels)
+       (channel
+	;; Make this directory itself a Guix channel.
+	(name 'guix-config)
+	(url "file:///home/inigo/guix-config")
+	(branch "main")))
